70ce3c541ed55fe1fbbad2f9323a9aed3f4ccf5e
RaplePoli <raphael12poligratis@gmail.com>
2025-09-16 09:12:53 +0800

    Add files via upload

---
+++ Denics' Codes/Administrator/Denics/Homepages/Program.cs
@@ -0,0 +1,22 @@
+/*
+
+using System.IO;
+using System.Windows.Forms; // Needed for MessageBox
+
+namespace Denics.Homepages
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Homepage());
+        }
+    }
+}
+
+*/
