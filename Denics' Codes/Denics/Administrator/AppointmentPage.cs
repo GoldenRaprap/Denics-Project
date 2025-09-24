6ddb95db6c1b3483b94ff54b245397a03d5cbd5c
RaplePoli <raphael12poligratis@gmail.com>
2025-09-24 08:52:04 +0800

    Update AppointmentPage.cs

--- Denics' Codes/Denics/Administrator/AppointmentPage.cs
+++ Denics' Codes/Denics/Administrator/AppointmentPage.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,7 +13,7 @@ namespace Denics.Administrator
 {
     public partial class AppointmentPage : Form
     {
-        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"D:\\Denics Project\\Denics' Database\\Denics_db.mdf\";Integrated Security=True;Connect Timeout=30");
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\1\\Downloads\\Denics' Database\\Denics_db.mdf\";Integrated Security=True;Connect Timeout=30");
         SqlCommand cmd;
 
         public AppointmentPage()
@@ -24,7 +24,26 @@ namespace Denics.Administrator
 
         private void AppointmentPage_Load(object sender, EventArgs e)
         {
-
+            try
+            {
+                con.Open();
+                SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM Appointments", con);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                OverallAppointmentTable.DataSource = dt;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("SQL Error: " + ex.Message, "Database Error");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("General Error: " + ex.Message, "Error");
+            }
+            finally
+            {
+                con.Close();
+            }
         }
     }
 }
